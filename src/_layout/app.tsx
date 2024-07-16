d8374b54901c3ebea2f79b62c39b848b5e483362
joaovfog <j.vitor48@outlook.com>
2024-07-16 18:55:42 -0300

    feat: layout page

--- src/_layout/app.tsx
+++ src/_layout/app.tsx
@@ -1,5 +1,14 @@
 import { Outlet } from "react-router-dom"
+import { Content, MainContainer } from "./styles"
+import { Header } from "../components"
 
 export const AppLayout = () => {
-    return <Outlet />
+    return (
+        <MainContainer>
+            <Header />
+            <Content>
+                <Outlet />
+            </Content>
+        </MainContainer>
+    )
 }
---
+++ src/_layout/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 2rem;
+  padding-top: 1.5rem;
+`
